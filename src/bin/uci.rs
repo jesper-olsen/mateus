2955876f7246ec4951f5e38ec037a96e4a944dd0
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2024-12-15 08:40:54 -0600

    +xboard

--- src/bin/uci.rs
+++ src/bin/uci.rs
@@ -1,4 +1,7 @@
-// UCI chess engine - to be used with a UCI GUI such as 
+// UCI chess engine - to be used with a UCI GUI such as
+// https://www.gnu.org/software/xboard/
+//    $ brew install xboard
+//    $ xboard -fcp target -fd . -fUCI
 // http://www.pearlchess.com/
 // http://www.cutechess.com/
 // and others
@@ -9,10 +12,13 @@
 //
 
 use std::io::{self, BufRead, Write};
-use std::sync::{Arc, Mutex, atomic::{AtomicBool, Ordering}};
+use std::sync::mpsc::{self, Sender};
+use std::sync::{
+    atomic::{AtomicBool, Ordering},
+    Arc, Mutex,
+};
 use std::thread;
 use std::time::Duration;
-use std::sync::mpsc::{self, Sender};
 
 fn main() {
     let stdin = io::stdin();
@@ -24,7 +30,12 @@ fn main() {
     thread::spawn(move || {
         for line in stdin.lock().lines() {
             let line = line.expect("Failed to read line");
-            handle_command(&line, tx_clone.clone(), Arc::clone(&is_searching), Arc::clone(&stop_search));
+            handle_command(
+                &line,
+                tx_clone.clone(),
+                Arc::clone(&is_searching),
+                Arc::clone(&stop_search),
+            );
         }
     });
 
@@ -33,30 +44,30 @@ fn main() {
     }
 }
 
-fn handle_command(command: &str, tx: Sender<String>, is_searching: Arc<AtomicBool>, stop_search: Arc<AtomicBool>) {
+fn handle_command(
+    command: &str,
+    tx: Sender<String>,
+    is_searching: Arc<AtomicBool>,
+    stop_search: Arc<AtomicBool>,
+) {
     match command {
         "uci" => {
             tx.send("id name MyChessEngine".to_string()).unwrap();
             tx.send("id author YourName".to_string()).unwrap();
             // option
             tx.send("uciok".to_string()).unwrap();
-        },
-        "debug on" => {
         }
-        "debug off" => {
-        }
+        "debug on" => {}
+        "debug off" => {}
         "isready" => {
             tx.send("readyok".to_string()).unwrap();
-        },
-        cmd if cmd.starts_with("setoption name ") => {
         }
-        "register later" => {
-        }
-        cmd if cmd.starts_with("register name") => {
-        }
+        cmd if cmd.starts_with("setoption name ") => {}
+        "register later" => {}
+        cmd if cmd.starts_with("register name") => {}
         "ucinewgame" => {
             // Initialize a new game
-        },
+        }
         cmd if cmd.starts_with("position") => {
             let parts: Vec<&str> = cmd.split_whitespace().collect();
             if parts.len() > 1 && parts[1] == "startpos" {
@@ -70,7 +81,7 @@ fn handle_command(command: &str, tx: Sender<String>, is_searching: Arc<AtomicBoo
                     // Apply each move
                 }
             }
-        },
+        }
         cmd if cmd.starts_with("go") => {
             if !is_searching.load(Ordering::SeqCst) {
                 is_searching.store(true, Ordering::SeqCst);
@@ -86,18 +97,16 @@ fn handle_command(command: &str, tx: Sender<String>, is_searching: Arc<AtomicBoo
                     is_searching_clone.store(false, Ordering::SeqCst);
                 });
             }
-        },
+        }
         "stop" => {
             stop_search.store(true, Ordering::SeqCst);
-        },
-        "ponderhit" => {
-        },
+        }
+        "ponderhit" => {}
         "quit" => {
             std::process::exit(0);
-        },
+        }
         _ => {
             // unknown command
         }
     }
 }
-
