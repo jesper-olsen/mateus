f1adea4f08f7b103c10b969c8073942d1ad7fe3c
Jesper Olsen <43079279+jesper-olsen@users.noreply.github.com>
2025-03-27 14:01:16 -0600

    bench: +move_gen +in_check +pawn_structure

--- benches/bn.rs
+++ benches/bn.rs
@@ -1,15 +1,43 @@
 use criterion::{Criterion, black_box, criterion_group, criterion_main};
+use puccinia_s_checkmate::benchmark::BRATKO_KOPEC;
 use puccinia_s_checkmate::mgen::*;
-
-fn move_gen() -> Vec<Move> {
-    let fen = "1k1r4/pp1b1R2/3q2pp/4p3/2B5/4Q3/PPP2B2/2K5 b - -"; // Bratko-Kopec Pos 1
-    let board = Board::from_fen(fen);
-    board.moves(false, false, None)
-}
+use puccinia_s_checkmate::val::Colour;
 
 fn bench_move_gen(c: &mut Criterion) {
-    c.bench_function("mgen BK1", |b| b.iter(|| move_gen()));
+    let fen = BRATKO_KOPEC[0].0;
+    let board = Board::from_fen(fen);
+    c.bench_function("mgen BK1", |b| {
+        b.iter(|| black_box(board.moves(false, false, None)))
+    });
 }
 
-criterion_group!(benches, bench_move_gen);
+fn bench_in_check(c: &mut Criterion) {
+    let fen = BRATKO_KOPEC[0].0;
+    let board = Board::from_fen(fen);
+    c.bench_function("in_check BK1", |b| {
+        b.iter(|| black_box(board.in_check(Colour::White)))
+    });
+}
+
+fn bench_mobility(c: &mut Criterion) {
+    let fen = BRATKO_KOPEC[0].0;
+    let board = Board::from_fen(fen);
+    c.bench_function("mobility BK1", |b| b.iter(|| black_box(board.mobility())));
+}
+
+fn bench_pawn_structure(c: &mut Criterion) {
+    let fen = BRATKO_KOPEC[0].0;
+    let board = Board::from_fen(fen);
+    c.bench_function("pawn_structure BK1", |b| {
+        b.iter(|| black_box(board.score_pawn_structure()))
+    });
+}
+
+criterion_group!(
+    benches,
+    bench_move_gen,
+    bench_in_check,
+    bench_mobility,
+    bench_pawn_structure
+);
 criterion_main!(benches);
